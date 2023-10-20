32a18daac16c59511c32423d8a492261e9ca29ea
Haritha-Kotapati <n01548934@humber.ca>
2023-10-20 10:06:09 -0400

    CORS updated

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,21 @@ mongoDB();
 app.use(bodyParser.json());
 
 //Use the CORS middleware to handle CORS headers
-app.use(cors());
+// Allow requests from the Vercel site and other origins
+const allowedOrigins = ['https://food-menu-cms.vercel.app', 'http://localhost:3000'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
 
 app.use((req,res,next) => {
     res.header("Access-Control-Allow-Origin","http://localhost:3000");
