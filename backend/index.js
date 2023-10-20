7c38fed0eb096c7113636bdc8aaa87d89a4ecdbb
Haritha-Kotapati <n01548934@humber.ca>
2023-10-20 10:44:14 -0400

    Cors up

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cors(corsOptions));
 
 
 app.use((req,res,next) => {
-    res.header("Access-Control-Allow-Origin","http://localhost:3000");
+    res.header("Access-Control-Allow-Origin","https://food-menu-cms.vercel.app");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
